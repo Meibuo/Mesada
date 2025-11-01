fcdac8a1270c7fd454d0d16beecb7cdef379ff18
Meibuo <willfernandu@gmail.com>
2025-11-01 18:59:50 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def dashboard():
     return render_template('dashboard.html', mesadas=mesadas)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
